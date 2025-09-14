f27a52ed4cb31aca07cbedaa1a1f6b2167b44917
Selçuk Özdemir <55selcukozdemir@gmail.com>
2025-09-14 11:34:44 +0000

    vs code intellisense working

--- main/main.c
+++ main/main.c
@@ -7,6 +7,7 @@
 #include "esp_log.h"
 #include "esp_system.h"
 #include "driver/uart.h"
+#include "driver/gpio.h"
 
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
@@ -35,6 +36,22 @@ void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 
 void micro_ros_task(void * arg)
 {
+
+	// //zero-initialize the config structure.
+    // gpio_config_t io_conf = {};
+    // //disable interrupt
+    // io_conf.intr_type = GPIO_INTR_DISABLE;
+    // //set as output mode
+    // io_conf.mode = GPIO_MODE_OUTPUT;
+    // //bit mask of the pins that you want to set,e.g.GPIO18/19
+    // io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    // //disable pull-down mode
+    // io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    // //disable pull-up mode
+    // io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    // //configure GPIO with the given settings
+    // gpio_config(&io_conf);
+
 	rcl_allocator_t allocator = rcl_get_default_allocator();
 	rclc_support_t support;
 
